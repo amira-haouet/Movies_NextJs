9be3ebe42396ca51068219c96ee3098fdff6db58
Amira.Haouet <haouetamira@gmail.com>
2024-11-21 15:06:34 +0100

    init dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Assurez-vous que le mode sombre est activé
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}', // Pour les fichiers dans le dossier "pages"
+    './components/**/*.{js,ts,jsx,tsx}', // Pour les composants
+    './app/**/*.{js,ts,jsx,tsx}' // Si vous utilisez le dossier "app"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)', // Assurez-vous de déclarer vos couleurs personnalisées
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [],
+};
