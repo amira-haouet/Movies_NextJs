6e8d83583429461142da2c49eab898e172bd1355
Amira.Haouet <haouetamira@gmail.com>
2024-11-21 09:41:58 +0100

    Add right url

--- app/utils/sideItem.ts
+++ app/utils/sideItem.ts
@@ -1,38 +1,37 @@
 import { Film, Smile, Tv, User } from "lucide-react"
 
 export const movieItems = [
-    {
-      title: "Now Playing",
-      url: "#",
-      icon: Film,
-    },
-    {
-      title: "Popular",
-      url: "#",
-      icon: User,
-    },
-    {
-      title: "Top Rated",
-      url: "#d",
-      icon: Smile,
-    },
-  ]
-
+  {
+    title: "Now Playing",
+    url: "api/movies/now-playing", 
+    icon: Film,
+  },
+  {
+    title: "Popular",
+    url: "api/movies/popular", 
+    icon: User,
+  },
+  {
+    title: "Top Rated",
+    url: "api/movies/top-rated", 
+    icon: Smile,
+  },
+];
 
 export const TVShowsItems = [
-    {
-      title: "On The Air",
-      url: "#",
-      icon: Tv,
-    },
-    {
-      title: "Popular",
-      url: "#",
-      icon: User,
-    },
-    {
-      title: "Top Rated",
-      url: "#",
-      icon: Smile,
-    },
-  ]
+  {
+    title: "On The Air",
+    url: "api/shows/on-the-air", 
+    icon: Tv,
+  },
+  {
+    title: "Popular",
+    url: "api/shows/popular", 
+    icon: User,
+  },
+  {
+    title: "Top Rated",
+    url: "api/shows/top-rated", 
+    icon: Smile,
+  },
+];
