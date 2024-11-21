6082da39feaef2fa4dad7f6ab8964f042c485bc4
Amira.Haouet <haouetamira@gmail.com>
2024-11-21 14:45:18 +0100

    Fix bug mentionner the right entity

--- components/ui/MediaGridMovie.tsx
+++ components/ui/MediaGridMovie.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import Image from 'next/image';
+import { Movie } from "@/app/entities/Movie";
+
 
 interface MediaGridProps {
   url: string;
@@ -9,7 +11,7 @@ interface MediaGridProps {
 }
 
 export default function MediaGrid({ url, title }: MediaGridProps) {
-  const [media, setMedia] = useState<any[]>([]);
+  const [media, setMedia] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -17,7 +19,7 @@ export default function MediaGrid({ url, title }: MediaGridProps) {
       try {
         const res = await fetch(url);
         if (!res.ok) throw new Error("Erreur lors de la récupération des données");
-        const data = await res.json();
+        const data: Movie[] = await res.json(); // Use the Movie interface for the API response
         setMedia(data);
       } catch (error) {
         console.error(error);
@@ -37,26 +39,24 @@ export default function MediaGrid({ url, title }: MediaGridProps) {
     <div className="p-4">
       {title && <h1 className="text-2xl font-bold mb-6">{title}</h1>}
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8">
-        {media.map((item) => (
+        {media.map((movie) => (
           <div
-            key={item.id}
+            key={movie.id}
             className="bg-white rounded-lg shadow-md overflow-hidden"
             style={{ maxWidth: "200px" }}
           >
             <Image
-              src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
-              alt={item.name || item.original_name || 'No title available'}
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title || 'No title available'}
               width={500}
               height={750}
               className="w-full h-auto"
             />
             <div className="p-2">
-              <h2 className="text-sm font-bold">
-                {item.title || item.original_title || item.name}
-              </h2>
-              <p className="text-xs text-gray-600 line-clamp-3">{item.overview}</p>
+              <h2 className="text-sm font-bold">{movie.title}</h2>
+              <p className="text-xs text-gray-600 line-clamp-3">{movie.overview}</p>
               <p className="mt-2 text-sm font-bold text-yellow-500">
-                Rating: {item.vote_average}
+                Rating: {movie.vote_average}
               </p>
             </div>
           </div>
