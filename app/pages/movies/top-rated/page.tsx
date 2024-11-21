eb47bd77e9f96ca695d0642b7a90eed9b40193d4
Amira.Haouet <haouetamira@gmail.com>
2024-11-21 18:42:13 +0100

    error fix

--- app/pages/movies/top-rated/page.tsx
+++ app/pages/movies/top-rated/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import GridMovie from "@/app/pages/movies/components/GridMovie";
 import { useFetchMovies } from "../useCase/useFetchMovies";
-import GridMovie from "@/app/pages/movies/components/GridMovie";
 
 export default function TopRatedMoviesPage() {
     const { movies, isLoading} = useFetchMovies("/api/movies/top-rated")
