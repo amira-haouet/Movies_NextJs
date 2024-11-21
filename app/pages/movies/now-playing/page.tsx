6ad50817375476ab178fab7f87b17125d1464d40
Amira.Haouet <haouetamira@gmail.com>
2024-11-21 18:39:59 +0100

    rename grid

--- app/pages/movies/now-playing/page.tsx
+++ app/pages/movies/now-playing/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import MediaGrid from "@/app/pages/movies/components/GridMovie";
+import GridMovie from "../components/GridMovie";
 import { useFetchMovies } from "../useCase/useFetchMovies";
 
 export default function NowPlayingMoviesPage() {
     const { movies, isLoading} = useFetchMovies("/api/movies/now-playing")
-    return <MediaGrid movies={movies} isLoading = {isLoading} />;
+    return <GridMovie movies={movies} isLoading = {isLoading} />;
 }
--- app/pages/movies/popular/page.tsx
+++ app/pages/movies/popular/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import MediaGrid from "@/app/pages/movies/components/GridMovie";
+import GridMovie from "../components/GridMovie";
 import { useFetchMovies } from "../useCase/useFetchMovies";
 
 export default function PopularMoviesPage() {
     const { movies, isLoading} = useFetchMovies("/api/movies/popular")
-    return <MediaGrid movies={movies} isLoading = {isLoading} />;
+    return <GridMovie movies={movies} isLoading = {isLoading} />;
 }
