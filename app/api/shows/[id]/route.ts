6f5c40fedef4f6a7d614dae532c37f6c05553b84
Amira.Haouet <haouetamira@gmail.com>
2024-11-27 14:43:59 +0100

    route details

---
+++ app/api/shows/[id]/route.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  if (!id) {
+    return NextResponse.json({ error: "L'ID de l'émission est requis." }, { status: 400 });
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.TMDB_API_KEY}&language=fr-FR`
+    );
+
+    if (!response.ok) {
+      throw new Error('Erreur lors de la récupération des détails de l\'émission');
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.error('Erreur API :', error);
+    return NextResponse.json({ error: 'Erreur interne' }, { status: 500 });
+  }
+}
