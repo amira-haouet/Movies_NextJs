65def8e8f261f6a97b3554fd6a7595020bfaf812
chaymaghrab <83771589+chaymaghrab@users.noreply.github.com>
2024-11-27 14:46:34 +0100

    fix urls

--- app/dashboard/movies/now-playing/page.tsx
+++ app/dashboard/movies/now-playing/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import GridMovie from "@/app/pages/movies/components/GridMovie";
+import GridMovie from "../components/GridMovie";
 import { useFetchMovies } from "../useCase/useFetchMovies";
 
 export default function NowPlayingMoviesPage() {
--- app/utils/sideItem.ts
+++ app/utils/sideItem.ts
@@ -3,17 +3,17 @@ import { Film, Smile, Tv, User } from "lucide-react"
 export const movieItems = [
   {
     title: "Now Playing",
-    url: "/pages/movies/now-playing", 
+    url: "/dashboard/movies/now-playing", 
     icon: Film,
   },
   {
     title: "Popular",
-    url: "/pages/movies/popular", 
+    url: "/dashboard/movies/popular", 
     icon: User,
   },
   {
     title: "Top Rated",
-    url: "/pages/movies/top-rated", 
+    url: "/dashboard/movies/top-rated", 
     icon: Smile,
   },
 ];
@@ -21,17 +21,17 @@ export const movieItems = [
 export const TVShowsItems = [
   {
     title: "On The Air",
-    url: "/pages/shows/on-the-air", 
+    url: "/dashboard/shows/on-the-air", 
     icon: Tv,
   },
   {
     title: "Popular",
-    url: "/pages/shows/popular", 
+    url: "/dashboard/shows/popular", 
     icon: User,
   },
   {
     title: "Top Rated",
-    url: "/pages/shows/top-rated", 
+    url: "/dashboard/shows/top-rated", 
     icon: Smile,
   },
 ];
